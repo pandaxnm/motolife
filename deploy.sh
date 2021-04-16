4959333d49b380ebdf493d5382e7d6a697f22092
xiongchaojie <023xcj@gmail.com>
2021-04-16 10:33:24 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "\033[32m ===== 构建 ===== \033[0m"
-hexo g
-
-git add .
-git commit -m "update"
-
-echo "\033[32m ===== 推送到远程仓库 ===== \033[0m"
-git push
+#echo "\033[32m ===== 构建 ===== \033[0m"
+#hexo g
+#
+#git add .
+#git commit -m "update"
+#
+#echo "\033[32m ===== 推送到远程仓库 ===== \033[0m"
+#git push
 echo "\033[32m ===== 发布网站 ===== \033[0m"
 curl http://106.13.43.91:8888/hook?access_key=wQb8qzDNuOGhLQi8IilBau4jnuBRQiiHmXbfF0r6iamKb6wy
 echo "\n"
