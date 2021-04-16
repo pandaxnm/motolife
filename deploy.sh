826e019aee2f9992838c27806dc25b59b8fd7908
xiongchaojie <023xcj@gmail.com>
2021-04-16 10:22:26 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,5 @@ echo "\033[32m ===== 推送到远程仓库 ===== \033[0m"
 git push
 echo "\033[32m ===== 发布网站 ===== \033[0m"
 curl http://106.13.43.91:8888/hook?access_key=wQb8qzDNuOGhLQi8IilBau4jnuBRQiiHmXbfF0r6iamKb6wy
+echo "\n"
 echo "\033[32m ===== 完成 ===== \033[0m"
